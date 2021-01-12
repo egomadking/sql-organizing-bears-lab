d3fa79e5b8b0c20d4210bfbd82043814825165af
Roy Mosby <ego.madking@gmail.com>
2021-01-12 13:46:59 -0500

    commit tree a3e9993760f88169b47d2e5a452e5706a5213121

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (1, "Mr. Chocolate", 6, "M", "brown", "lazy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (2, "Rowdy", 10, "M", "brown", "hostile", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (3, "Tabitha", 3, "F", "black", "wary", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (
+    4,
+    "Sergeant Brown",
+    12,
+    "F",
+    "black",
+    "curious",
+    0
+  );
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (5, "Melissa", 9, "F", "brown", "aggressive", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (6, "Grinch", 15, "M", "black", "sullen", 0);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (7, "Wendy", 8, "F", "brown", "curious", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES (8, NULL, 12, "M", "brown", "peaceful", 0);
